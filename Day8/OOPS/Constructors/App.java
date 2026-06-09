a649d953fe294f81a51620b28f9cae6750081e1a
pankajoberoi <poberoi.ca19@chitkara.edu.in>
2026-06-09 11:34:28 +0530

    Updated

---
+++ Day8/OOPS/Constructors/App.java
@@ -0,0 +1,17 @@
+package Day8.OOPS.Constructors;
+
+public class App {
+    public static void main(String[] args) {
+
+        HDFC c1 = new HDFC();
+        HDFC c2 = new HDFC();
+        HDFC c3 = new HDFC();
+        HDFC c4 = new HDFC();
+
+        c4.name = "Rupa";
+        System.out.println(c4.name);
+        System.out.println(c3.AccNo);
+        System.out.println(c2.balance);
+
+    }
+}
